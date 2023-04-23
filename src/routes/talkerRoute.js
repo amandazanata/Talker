a569c7b1668c5ec1c66ae297c6aaf8f3c0fa5b8f
Amanda Zanata <amandazanata46@gmail.com>
2023-04-22 21:24:14 -0300

    corrige linter

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const utilsFunction = require('../utils');
 const talkers = require('../talker.json');
 
 const route = express.Router();
 
-route.get('/', async (_req, res) => {
+// Crie o endpoint GET /talker
+route.get('/', (_req, res) => {
   // const array = [];
   const array = talkers.splice(0);
   if (!talkers) {
@@ -13,14 +13,20 @@ route.get('/', async (_req, res) => {
   return res.status(200).json(array);
 });
 
-route.get('/:id', async (req, res) => {
-  // const talkers = await readFile();
+// Crie o endpoint GET /talker/:id
+route.get('/:id', (req, res) => {
   const speaker = talkers.find((talker) => talker.id === Number(req.params.id));
     
-    if (speaker) {
-      return res.status(200).json(speaker);
-    }
+  if (speaker) {
+    return res.status(200).json(speaker);
+  }
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  });
+});
+
+// requisito 5
+
+// requisito 6 e 7
+
+// requisito 8
 
 module.exports = route;
